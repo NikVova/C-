a0f6c1f59b9d0acd893552171b26d2473c3db8b3
Vladimir Nikolaev <isak25@mail.ru>
2022-01-04 09:53:21 +0200

    add examples005_ifelse

---
+++ Examples005_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "сева"){
+    Console.WriteLine("Ура, это же Всеволод !!!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
